6a18ae825fd7ae3ec3644f8e194348f7823fc1c3
unknown <stha.suman327@gmail.com>
2022-05-04 21:02:36 +0545

    Intial commit

---
+++ base/api/views.py
@@ -0,0 +1,11 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+def getRoutes(request):
+    routes = [
+        'GET /api',
+        'GET /api/rooms',
+        'GET /api/rooms/:id' 
+    ]
+    return JsonResponse(routes, safe=False)
